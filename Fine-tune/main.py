49d55662fa5fd0f9956e388b21a7c957b578a4fb
DanQuang <tquangdan03@gmail.com>
2024-06-19 20:28:04 +0700

    update

---
+++ Fine-tune/main.py
@@ -0,0 +1,30 @@
+import os
+import yaml # type: ignore
+import argparse
+import logging
+from typing import Text
+
+from task.train import Train_Task
+# from task.infer import Infer_Task
+
+def main(config_path: Text) -> None:
+    logging.basicConfig(level=logging.INFO)
+
+    with open(config_path) as f:
+        config = yaml.safe_load(f)
+
+    logging.info("Training started...")
+    Train_Task(config).train()
+
+    logging.info("Train complete")
+
+    # logging.info("Evaluating...")
+    # Infer_Task(config).predict()
+    # logging.info("Task done!!!")
+
+if __name__ == "__main__":
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--config', dest='config', required=True)
+    args = args_parser.parse_args()
+    
+    main(args.config)
--- Fine-tune/task/train.py
+++ Fine-tune/task/train.py
@@ -3,6 +3,8 @@ from torch import nn, optim
 import os
 from data_utils.load_data import Load_Data
 from models.classifier_model import Classifier_Model
+from transformers import get_scheduler
+from tqdm.auto import tqdm
 from utils.utils import count_trainable_parameters
 
 
@@ -13,12 +15,73 @@ class Train_Task:
         self.save_path = config["save_path"]
         self.patience = config["patience"]
         self.dropout = config["dropout"]
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
 
         # load data
         self.load_data = Load_Data(config)
 
         # Load model
-        self.model = Classifier_Model(config)
+        self.model = Classifier_Model(config).to(self.device)
 
         # Load loss function and optimizer
-        self.loss = 
+        self.loss = nn.CrossEntropyLoss()
+        self.optimizer = optim.AdamW(self.model.parameters(), lr= self.learning_rate)
+
+        self.train_dataloader, self.dev_dataloader = self.load_data.load_train_dev()
+
+        self.num_training_steps = self.num_epochs * len(self.train_dataloader)
+
+        self.lr_scheduler = get_scheduler(name= "linear",
+                                          optimizer= self.optimizer,
+                                          num_warmup_steps= 0,
+                                          num_training_steps= self.num_training_steps)
+    
+    def train(self):
+        if not os.path.exists(self.save_path):
+            os.makedirs(self.save_path)
+
+        last_model = "Pretrained_last_model.pth"
+        best_model = "Pretrained_best_model.pth"
+
+        if os.path.exists(os.path.join(self.save_path, last_model)):
+            checkpoint = torch.load(os.path.join(self.save_path, last_model))
+            self.model.load_state_dict(checkpoint["model_state_dict"])
+            self.optimizer.load_state_dict(checkpoint["optim_state_dict"])
+            print("Load the last model")
+            initial_epoch = checkpoint["epoch"] + 1
+            print(f"Continue training from epoch {initial_epoch}")
+
+        else:
+            initial_epoch = 0
+            print("First time training!!!")
+
+        if os.path.exists(os.path.join(self.save_path, best_model)):
+            checkpoint = torch.load(os.path.join(self.save_path, best_model))
+            best_score = checkpoint['score']
+        else:
+            best_score = 0.
+
+        threshold = 0
+        print(f"The Pretrained model has {count_trainable_parameters(model= self.model)} trainable parameters")
+        progress_bar = tqdm(range(self.num_training_steps))
+        self.model.train()
+        for epoch in range(initial_epoch, initial_epoch + self.num_epochs):
+            epoch_loss = 0
+            for _, item in enumerate(self.train):
+                texts, labels = item["text"], item["label"]
+
+                self.optimizer.zero_grad()
+
+                outputs = self.model(texts)
+
+                loss = self.loss(outputs, labels)
+
+                self.optimizer.step()
+                self.lr_scheduler.step()
+                progress_bar.update(1)
+
+                epoch_loss += loss.item()
+            
+            train_loss = epoch_loss / len(self.train_dataloader)
+            print(f"Epoch {epoch}:")
+            print(f"Train loss: {train_loss:.5f}")
